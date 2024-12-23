1aa4919ba88350f0879870603004f60f5330cec4
naviyaboy123 <naviyab979@gmail.com>
2024-12-23 15:29:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=fEEFCBTC#l3w2E-uwB-xpw2kywkdnQan4I0aWQ73PPkoiIsJLceg",
-MONGODB: process.env.MONGODB || "mongodb://mongo:TchxmzYPWNyUdaEJKSpMfJJuwMPHHLLg@junction.proxy.rlwy.net:33420",
+MONGODB: process.env.MONGODB || "mongodb://mongo:SvRJErPhTDJcdEtCHLEafQpNhrnjLzem@junction.proxy.rlwy.net:49890",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "8748dc2e"
 };
 

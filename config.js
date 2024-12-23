56fc25112ae13edfb74182a0cbd63db3ccc0fc10
naviyaboy123 <naviyab979@gmail.com>
2024-12-23 15:19:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=XcERTIQZ#EUvbAO9_d3TP6wUahaAIkENjvTp5WYiPx0PDEFVFxrw",
-MONGODB: process.env.MONGODB || "mongodb://mongo:hDtscvzygtxOuBnrNGaNkRbsgziCJjjq@junction.proxy.rlwy.net:22030",
+SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=fEEFCBTC#l3w2E-uwB-xpw2kywkdnQan4I0aWQ73PPkoiIsJLceg",
+MONGODB: process.env.MONGODB || "mongodb://mongo:TchxmzYPWNyUdaEJKSpMfJJuwMPHHLLg@junction.proxy.rlwy.net:33420",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "8748dc2e"
 };
 
